45404868dadd1da6a8446409f5feaa9236232acb
Saza-ku <the.latticeheart@gmail.com>
2023-05-19 00:19:05 +0900

    add todo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const accessToken = 'あなたのチャネルアクセストークン'
 const lineReplyUrl = 'https://api.line.me/v2/bot/message/reply'
+const spreadsheetId = 'スプレッドシートのID'
 
 function doPost(e) {
     // イベントの中から必要な情報を取得
@@ -7,6 +8,17 @@ function doPost(e) {
     let json = data.events[0]
     let replyToken = json.replyToken
     let userMessage = json.message.text
+    let spreadSheetApp = SpreadsheetApp.openById(spreadsheetId)
+    let sheet = spreadSheetApp.getSheets()[0]
+
+    // メッセージを解析
+    let command = userMessage.split("\n")[0]
+    let todo = userMessage.split("\n")[1]
+
+    // タスクを追加する
+    if (command === "追加") {
+      sheet.appendRow([todo])
+    }
 
     // 返信メッセージを作る
     let replyMessage = userMessage
